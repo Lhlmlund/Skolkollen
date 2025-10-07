549bd0398b6d476ea0d1a940e4e6cff96b597bf0
Alexander Pettersson <alexpet5888@gmail.com>
2025-10-08 00:15:15 +0200

    add Documentation

--- backend/controllers/schoolController.js
+++ backend/controllers/schoolController.js
@@ -1,5 +1,11 @@
 const schoolService = require('../services/schoolService.js')
 
+/**
+ * Retrieves all schools from the database.
+ * @param req
+ * @param res
+ * @returns {Promise<*>} json with [schools] or error message
+ */
 async function getSchools(req, res){
     try {
         const result = await schoolService.getSchools()
--- backend/services/schoolService.js
+++ backend/services/schoolService.js
@@ -1,5 +1,10 @@
 import db from "../dbConnection.js";
 
+/**
+ * Retrieves all schools from the database.
+ * @returns {Promise<Array<object>>} A list of schools.
+ * @throws {Error} If the database query fails.
+ */
  export async function getSchools() {
     try {
         const [rows] = await db.query('SELECT * FROM school')
