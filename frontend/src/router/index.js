a5ce9b7690fd24d4948d7d17d8122b9233ea8787
Linus Holmlund <linus_holmlund@outlook.com>
2025-10-15 23:26:28 +0200

    renamed page components to PascalCase for consistency

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,14 +3,14 @@ import Home from '../pages/Home.vue'
 import About from '../pages/About.vue'
 import AddSchool from "../pages/AddSchool.vue";
 import SchoolList from "../pages/SchoolList.vue";
-import login from "../pages/login.vue";
+import Login from "../pages/Login.vue";
 
 const routes = [
   { path: '/', name: 'home', component: Home },
   { path: '/about', name: 'about', component: About },
   { path: '/add-school', name: 'addSchool', component: AddSchool },
   { path: '/school-list', name: 'schoolList', component: SchoolList },
-  { path: '/login', name: 'login', component: login }
+  { path: '/Login', name: 'login', component: Login }
 
 ]
 
