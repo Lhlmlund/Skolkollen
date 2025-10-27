e37a49ffd411d7f8ce5893f6edb78a359095311d
Alexander <alexpet5888@gmail.com>
2025-10-27 11:08:04 +0100

    Service and controller init

---
+++ backend/controllers/UserController.js
@@ -0,0 +1,19 @@
+export async function getUsers(){
+
+}
+
+export async function getUserById(){
+
+}
+
+export async function registerUserById(){
+
+}
+
+export async function updateUserById(){
+
+}
+
+export async function deleteUserById(){
+
+}
---
+++ backend/services/userService.js
@@ -0,0 +1,30 @@
+import { prisma } from '../prismaClient.js'
+
+export async function listUsers(){
+    return await prisma.user.findMany()
+}
+
+export async function getUserById(id){
+    return await prisma.user.findUnique({
+        where: { id }
+    })
+}
+
+export async function registerUser(data){
+    return await prisma.user.create({
+        data
+    })
+}
+
+export function updateUserById(id, data) {
+    return prisma.user.update({
+        where: { id: Number(id) },
+        data,
+    });
+}
+
+export function deleteUserById(id) {
+    return prisma.user.delete({
+        where: { id: Number(id) },
+    });
+}
