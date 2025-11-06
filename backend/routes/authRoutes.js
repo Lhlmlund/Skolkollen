d5a2274792b1c58fa47253cd762de9c8ed9fdf85
Alexander Pettersson <alexpet5888@gmail.com>
2025-11-06 09:17:21 +0100

    refactor

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,7 +3,7 @@ import express from "express"
 import {authenticateToken} from "../middleware/authMiddleware.js";
 const router = express.Router();
 
-router.get('/login',loginUser)
+router.post('/login',loginUser)
 router.post('/register', registerUser)
 router.put('/update',authenticateToken, updateUserById)
 
